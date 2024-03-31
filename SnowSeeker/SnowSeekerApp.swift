17a1401af84871b412920d0e57070bd5b8c0a8be
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-04-01 02:08:14 +0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Насрулло Исмоилжонов on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
